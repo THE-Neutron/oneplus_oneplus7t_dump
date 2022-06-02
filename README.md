ad02ea6e1ed308929bafdd6da29e9f3561a8209e
root <root@fv-az246-791.py5defvocjqundwqxk0lumnltg.cx.internal.cloudapp.net>
2022-06-02 12:17:01 +0000

    Add extras for qssi-user 11 RKQ1.201022.002 2202112156 release-keys
    Signed-off-by: root <root@fv-az246-791.py5defvocjqundwqxk0lumnltg.cx.internal.cloudapp.net>

---
+++ README.md
@@ -0,0 +1,21 @@
+## qssi-user 11 RKQ1.201022.002 2202112156 release-keys
+- Manufacturer: oneplus
+- Platform: msmnile
+- Codename: OnePlus7T
+- Brand: OnePlus
+- Flavor: qssi-user
+- Release Version: 11
+- Id: RKQ1.201022.002
+- Incremental: 2202112156
+- Tags: release-keys
+- CPU Abilist: arm64-v8a,armeabi-v7a,armeabi
+- A/B Device: true
+- Locale: en-US
+- Screen Density: undefined
+- Fingerprint: OnePlus/OnePlus7T/OnePlus7T:11/RKQ1.201022.002/2202112156:user/release-keys
+- OTA version: 
+- Branch: qssi-user-11-RKQ1.201022.002-2202112156-release-keys
+- Repo: oneplus_oneplus7t_dump
+
+
+>Dumped by [Phoenix Firmware Dumper](https://github.com/DroidDumps/phoenix_firmware_dumper)
---
+++ join_split_files.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+cat boot.img.* 2>/dev/null >> boot.img
+rm -f boot.img.* 2>/dev/null
+cat system_ext/app/OPBreathMode/OPBreathMode.apk.* 2>/dev/null >> system_ext/app/OPBreathMode/OPBreathMode.apk
+rm -f system_ext/app/OPBreathMode/OPBreathMode.apk.* 2>/dev/null
+cat system_ext/app/OPWallpaperResources/OPWallpaperResources.apk.* 2>/dev/null >> system_ext/app/OPWallpaperResources/OPWallpaperResources.apk
+rm -f system_ext/app/OPWallpaperResources/OPWallpaperResources.apk.* 2>/dev/null
+cat system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null >> system_ext/priv-app/Settings/Settings.apk
+rm -f system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null
+cat system_ext/priv-app/OnePlusCamera/OnePlusCamera.apk.* 2>/dev/null >> system_ext/priv-app/OnePlusCamera/OnePlusCamera.apk
+rm -f system_ext/priv-app/OnePlusCamera/OnePlusCamera.apk.* 2>/dev/null
+cat system_ext/priv-app/OnePlusGallery/OnePlusGallery.apk.* 2>/dev/null >> system_ext/priv-app/OnePlusGallery/OnePlusGallery.apk
+rm -f system_ext/priv-app/OnePlusGallery/OnePlusGallery.apk.* 2>/dev/null
+cat product/app/Maps/Maps.apk.* 2>/dev/null >> product/app/Maps/Maps.apk
+rm -f product/app/Maps/Maps.apk.* 2>/dev/null
+cat product/app/Photos/Photos.apk.* 2>/dev/null >> product/app/Photos/Photos.apk
+rm -f product/app/Photos/Photos.apk.* 2>/dev/null
+cat product/app/LatinImeGoogle/LatinImeGoogle.apk.* 2>/dev/null >> product/app/LatinImeGoogle/LatinImeGoogle.apk
+rm -f product/app/LatinImeGoogle/LatinImeGoogle.apk.* 2>/dev/null
+cat product/app/YouTube/YouTube.apk.* 2>/dev/null >> product/app/YouTube/YouTube.apk
+rm -f product/app/YouTube/YouTube.apk.* 2>/dev/null
+cat product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null >> product/app/WebViewGoogle/WebViewGoogle.apk
+rm -f product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null
+cat product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null >> product/priv-app/GmsCore/GmsCore.apk
+rm -f product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null
+cat product/priv-app/Velvet/Velvet.apk.* 2>/dev/null >> product/priv-app/Velvet/Velvet.apk
+rm -f product/priv-app/Velvet/Velvet.apk.* 2>/dev/null
+cat recovery.img.* 2>/dev/null >> recovery.img
+rm -f recovery.img.* 2>/dev/null
+cat system/system/reserve/CanvasResources/CanvasResources.apk.* 2>/dev/null >> system/system/reserve/CanvasResources/CanvasResources.apk
+rm -f system/system/reserve/CanvasResources/CanvasResources.apk.* 2>/dev/null
+cat system/system/apex/com.android.art.release.apex.* 2>/dev/null >> system/system/apex/com.android.art.release.apex
+rm -f system/system/apex/com.android.art.release.apex.* 2>/dev/null
+cat system/system/apex/com.android.vndk.current.apex.* 2>/dev/null >> system/system/apex/com.android.vndk.current.apex
+rm -f system/system/apex/com.android.vndk.current.apex.* 2>/dev/null
